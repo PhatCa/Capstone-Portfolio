f73f587715cce26f2772dc1a344af4735251b2ba
PhatCa <phatca@hawaii.edu>
2023-04-12 17:16:57 -1000

    finish with portfolio

---
+++ function.js
@@ -0,0 +1,84 @@
+//sticky nav bar function
+window.onscroll = function () {
+  stickyNavbar();
+};
+
+function stickyNavbar() {
+  let navbar = document.getElementById("navbar");
+  if (window.pageYOffset > 0) {
+    navbar.style.position = "fixed";
+    navbar.style.top = "0";
+  } else {
+    navbar.style.position = "relative";
+    navbar.style.top = "";
+  }
+}
+
+// click and scroll down into section function
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  section.scrollIntoView({ behavior: "smooth" });
+}
+
+// vertical nav bar in aboutme
+const experienceButton = document.querySelector(
+  ".navbar-skill-button-experience"
+);
+const experienceInfo = document.querySelector(".experience");
+const educationButton = document.querySelector(
+  ".navbar-skill-button-education"
+);
+const educationInfo = document.querySelector(".education");
+const skillButton = document.querySelector(".navbar-skill-button-skill");
+const skillInfo = document.querySelector(".skill");
+
+educationButton.addEventListener("click", () => {
+  if (experienceInfo.classList.contains("show-education")) {
+    experienceInfo.classList.remove("show-education");
+  }
+  if (skillInfo.classList.contains("show-education")) {
+    skillInfo.classList.remove("show-education");
+  }
+  educationInfo.classList.toggle("show-education");
+});
+
+experienceButton.addEventListener("click", () => {
+  if (educationInfo.classList.contains("show-education")) {
+    educationInfo.classList.remove("show-education");
+  }
+  if (skillInfo.classList.contains("show-education")) {
+    skillInfo.classList.remove("show-education");
+  }
+  experienceInfo.classList.toggle("show-education");
+});
+
+skillButton.addEventListener("click", () => {
+  if (educationInfo.classList.contains("show-education")) {
+    educationInfo.classList.remove("show-education");
+  }
+  if (experienceInfo.classList.contains("show-education")) {
+    experienceInfo.classList.remove("show-education");
+  }
+
+  skillInfo.classList.toggle("show-education");
+});
+
+const typingEffect = document.querySelector("#typing-effect");
+const textToType = "Hi, my name is Phat!";
+const typingSpeed = 100;
+typingEffect.textContent = "";
+
+function animateTypingEffect() {
+  const currentText = typingEffect.textContent;
+
+  if (currentText === textToType) {
+    return;
+  }
+
+  const nextCharacter = textToType[currentText.length];
+  typingEffect.textContent = currentText + nextCharacter;
+
+  setTimeout(animateTypingEffect, typingSpeed);
+}
+
+animateTypingEffect();
